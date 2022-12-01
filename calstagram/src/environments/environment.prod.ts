5b35c474183f6e5f4246f378dfbb691801982e47
Seong Chan Cho <csc9808@gmail.com>
2022-12-01 17:17:00 -0500

    firebase setup

--- calstagram/src/environments/environment.prod.ts
+++ calstagram/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'calstagram-e0db0',
+    appId: '1:390343640039:web:ed44694f0ffbbf47e33342',
+    storageBucket: 'calstagram-e0db0.appspot.com',
+    apiKey: 'AIzaSyBpsUpB1u784Oq51lWn0L2LXi7Gl2gW3x8',
+    authDomain: 'calstagram-e0db0.firebaseapp.com',
+    messagingSenderId: '390343640039',
+    measurementId: 'G-3ML7WL0YQE',
+  },
   production: true
 };
--- calstagram/src/environments/environment.ts
+++ calstagram/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'calstagram-e0db0',
+    appId: '1:390343640039:web:ed44694f0ffbbf47e33342',
+    storageBucket: 'calstagram-e0db0.appspot.com',
+    apiKey: 'AIzaSyBpsUpB1u784Oq51lWn0L2LXi7Gl2gW3x8',
+    authDomain: 'calstagram-e0db0.firebaseapp.com',
+    messagingSenderId: '390343640039',
+    measurementId: 'G-3ML7WL0YQE',
+  },
   production: false
 };
 
